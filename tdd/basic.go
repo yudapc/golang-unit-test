a93ff462473b9a6a8f3b09376d9fa1625f74ec7f
Yuda Cogati <yuda.pc@gmail.com>
2022-09-27 20:29:07 +0700

    [tdd] add sample test with sub set and table test

--- tdd/basic.go
+++ tdd/basic.go
@@ -1,7 +1,18 @@
 package tdd
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func SayHello(name string) string {
 	return fmt.Sprintf("Hello %s, Welcome!", name)
+}
+
+func OddOrEven(value int) string {
+	criteria := math.Mod(float64(value), 2)
+	if criteria == 1 {
+		return fmt.Sprintf("%v is an odd number", value)
+	}
+	return fmt.Sprintf("%v is an even number", value)
 }
--- tdd/basic_test.go
+++ tdd/basic_test.go
@@ -10,3 +10,37 @@ func TestSayHello(t *testing.T) {
 	greeting := SayHello("Yuda")
 	assert.Equal(t, "Hello Yuda, Welcome!", greeting)
 }
+
+func TestOddOrEven(t *testing.T) {
+	assert.Equal(t, "45 is an odd number", OddOrEven(45))
+	assert.Equal(t, "42 is an even number", OddOrEven(42))
+}
+
+func TestOddOrEvenSubSet(t *testing.T) {
+	t.Run("When number in criteria == 1", func(t *testing.T) {
+		assert.Equal(t, "42 is an even number", OddOrEven(42))
+	})
+	t.Run("When number in criteria != 1", func(t *testing.T) {
+		assert.Equal(t, "45 is an odd number", OddOrEven(45))
+	})
+}
+
+func TestOddOrEvenTableTest(t *testing.T) {
+	tests := []struct {
+		input    int
+		expected string
+	}{
+		{
+			input:    42,
+			expected: "42 is an even number",
+		},
+		{
+			input:    45,
+			expected: "45 is an odd number",
+		},
+	}
+
+	for _, test := range tests {
+		assert.Equal(t, test.expected, OddOrEven(test.input))
+	}
+}
